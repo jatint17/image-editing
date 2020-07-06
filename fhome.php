1046b628a6c616ece70cc4bc18c9333743b293c2
Jatin Takkar <jatintakkar@ymail.com>
2020-07-06 16:35:59 +0530

    7th commit
    Improved login and logout features.

--- fhome.php
+++ fhome.php
@@ -1,20 +1,47 @@
 <?php session_start(); ?>
+
 <html>
 <script src="simpleimage.js"></script>
 <script src="filter.js"></script>
 <link rel="stylesheet" type="text/css" href="styleit.css">
 <head>
-  <h1>Jatin welcomes you to<br>
-      Image Filters in a Webpage</h1>
-</head>
-<body>
-  This is a website for applying desired filters to an image you upload.
-  <br>
-  <div id="one">
-  <a href="index.php">Home </a>
-  <a href="login.php">Login/Sign Up </a>
-  <a href="abs.php">About Us?</a>
-  </div>
+  <h1>
+  
+  <?php
+  
+  if(isset($_SESSION['user']))
+  {
+    $us=$_SESSION['user'];
+    echo "Jatin welcomes $us to<br>
+    Image Filters in a Webpage";
+
+    echo "</h1></head>
+    <body>
+    This is a website for applying desired filters to an image you upload.      
+    <br>
+      <div id='one'>
+      <a href='index.php'>Home </a>
+      <a href='logout.php'>Logout</a>
+      <a href='abs.php'>About Us?</a>
+      </div>";
+  }
+  else
+  {
+    echo "Jatin welcomes you to<br>
+    Image Filters in a Webpage";
+  
+    echo "</h1></head>
+    <body>
+    This is a website for applying desired filters to an image you upload.
+      <br>
+      <div id='one'>
+      <a href='index.php'>Home </a>
+      <a href='login.php'>Login/Sign Up </a>
+      <a href='abs.php'>About Us?</a>
+      </div>";
+  }?>
+
+  <body>
   <canvas id="fgc"></canvas>
   <canvas id="gsc"></canvas>
   <p>Please select an image :
--- home.php
+++ home.php
@@ -1,15 +1,20 @@
+<?php session_start();?>
+
 <html>
 <script src="simpleimage.js"></script>
 <script src="uploadfg.js"></script>
 <link rel="stylesheet" type="text/css" href="styleit.css">
 <head>
   <h1>
-  <?php session_start();
+  
+  <?php
+  
   if(isset($_SESSION['user']))
   {
-
-    echo "Jatin welcomes login to<br>
+    $us=$_SESSION['user'];
+    echo "Jatin welcomes $us to<br>
       Green Screen Background Changer in a Webpage";
+
     echo "</h1></head>
     <body>
       This is a website for changing the background of the green screen image you upload to the background you desire.
@@ -24,17 +29,18 @@
   {
     echo "Jatin welcomes you to<br>
     Green Screen Background Changer in a Webpage";
-  echo "</h1></head>
-  <body>
-  This is a website for changing the background of the green screen image you upload to the background you desire.
-  <br>
-  <div id='one'>
-  <a href='index.php'>Home </a>
-  <a href='login.php'>Login/Sign Up </a>
-  <a href='abs.php'>About Us?</a>
-  </div>";
-  }
-  ?>
+  
+    echo "</h1></head>
+    <body>
+      This is a website for changing the background of the green screen image you upload to the background you desire.
+      <br>
+      <div id='one'>
+      <a href='index.php'>Home </a>
+      <a href='login.php'>Login/Sign Up </a>
+      <a href='abs.php'>About Us?</a>
+      </div>";
+  }?>
+  
   <canvas id="fgc"></canvas>
   <canvas id="bgc"></canvas>
   <canvas id="gsc"></canvas>
